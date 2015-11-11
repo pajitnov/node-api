a7634f401fbeb1b694f7a13f2aaef732ec5103aa
pajitnov <alexei@pajitnov.info>
2015-11-10 23:39:55 -0500

    adding prelimenary files

---
+++ public/app/app.routes.js
@@ -0,0 +1,9 @@
+angular.module('appRoutes',['ngRoute'])
+
+.config(function($routeProvider, $locationProvider){
+	$routeProvider
+		.when('/',{
+			templateUrl: 'app/views/pages/home.html'
+
+		})
+});
