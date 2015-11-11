9fdf973af73ca414935fe26b5f1ab534e6fd5239
pajitnov <alexei@pajitnov.info>
2015-11-10 23:32:35 -0500

    adding the db connection

---
+++ server.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var config = require('./config');
+var mongoose = require('mongoose');
+var app = express();
+
+mongoose.connect(config.database, function(err){
+	if (err) {
+		console.log(err)
+	}
+	else {
+		console.log('Conected to the database')
+	}
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+
+app.use(express.static(__dirname + '/public'));
+
+var api = require('./app/routes/api')(api, express);
+app.use('/api',api);
+
+app.get('*', function(req,res){
+	res.sendFile(__dirname + '/public/app/views/index.html');
+})
+
+
+app.listen(config.port, function(err){
+   
+	if(err){
+		consaole.log(err);
+	} 
+
+	else {
+		console.log("listening on port 3000");
+	}
+
+});
